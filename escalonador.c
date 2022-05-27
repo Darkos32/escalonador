1c877460f4b19cd0b118c622080d57358e70c85e
Darkos32 <jorgeoliv32@gmail.com>
2022-05-27 15:00:45 -0300

    area de espera organizada por chegada

--- escalonador.c
+++ escalonador.c
@@ -7,42 +7,42 @@ typedef struct p
 {
     int pid;
     int tempoExecucao;
-    char* operacoesIO;
-    int* instantesIO;
+    char *operacoesIO;
+    int *instantesIO;
     int tempoChegada;
     int numeroIO;
 } processo;
-void stringOverflow(char* vetor){
-    int tam = strlen(vetor);
-}
-int* parseInstantesIO(char* instantesString,int numeroIO){
-    int i = 0,j=0,l=0,*instantesInt;
+
+int *parseInstantesIO(char *instantesString, int numeroIO)
+{
+    int i = 0, j = 0, l = 0, *instantesInt;
     char *numero;
-    numero = (char *)malloc(3*sizeof(char));//a string esta preparada a priori pra receber no maximo 3 digitos
-    if (numeroIO ==0)
+    numero = (char *)malloc(3 * sizeof(char)); // a string esta preparada a priori pra receber no maximo 3 digitos
+    if (numeroIO == 0)
     {
         return NULL;
     }
-    instantesInt = (int*) malloc(numeroIO*sizeof(int));
-    while (instantesString[i]!='\0')
+    instantesInt = (int *)malloc(numeroIO * sizeof(int));
+    while (instantesString[i] != '\0')
     {
-        
-        if (instantesString[i] == '['||instantesString[i] == ']'||instantesString[i] == ',')
+
+        if (instantesString[i] == '[' || instantesString[i] == ']' || instantesString[i] == ',')
         {
             i++;
             continue;
-        }else{
+        }
+        else
+        {
             numero[j] = instantesString[i];
             j++;
 
             while (instantesString[i + 1] != '\0' && isdigit(instantesString[i + 1]))
             {
-                
+
                 numero[j] = instantesString[i + 1];
                 j++;
                 i++;
             }
-            
         }
         instantesInt[l] = atoi(numero);
         l++;
@@ -51,23 +51,24 @@ int* parseInstantesIO(char* instantesString,int numeroIO){
     return instantesInt;
 }
 
-//Inicializa os atributos de um processo
-processo* setProcesso(char* linhaTabela){
-    processo * p;
-    char*token;
+// Inicializa os atributos de um processo
+processo *setProcesso(char *linhaTabela)
+{
+    processo *p;
+    char *token;
     int i = 0;
-    p = (processo *) malloc(sizeof(processo));   
-    token = strtok(linhaTabela," ");
-    while (token!=NULL)
+    p = (processo *)malloc(sizeof(processo));
+    token = strtok(linhaTabela, " ");
+    while (token != NULL)
     {
         switch (i)
         {
         case 0:
-            
+
             p->pid = atoi(token);
             break;
         case 1:
-            
+
             p->tempoExecucao = atoi(token);
             break;
         case 2:
@@ -77,7 +78,7 @@ processo* setProcesso(char* linhaTabela){
             p->operacoesIO = token;
             break;
         case 4:
-            p->instantesIO = parseInstantesIO(linhaTabela,p->numeroIO);
+            p->instantesIO = parseInstantesIO(linhaTabela, p->numeroIO);
             break;
         case 5:
             p->tempoChegada = atoi(token);
@@ -92,43 +93,66 @@ processo* setProcesso(char* linhaTabela){
     return p;
 }
 
-//Pega os processos descritos na tabela e os coloca num tipo de área de espera 
-processo** parseTabela(){
-    FILE *tabela = fopen("tabela.txt","r");
-    char * linha = NULL;
-    short int flag = 1;// marca se a primeira linha da tabela ainda não foi lida
+void bubbleSort(processo **vetor, int tam)
+{
+    if (tam < 1)
+    {
+        return;
+    }
+    for (size_t i = 0; i < tam-1; i++)
+    {
+        if (vetor[i]->tempoChegada > vetor[i + 1]->tempoChegada)
+        {
+
+            processo *temp = vetor[i];
+            vetor[i] = vetor[i + 1];
+            vetor[i + 1] = temp;
+        }
+        bubbleSort(vetor, tam - 1);
+    }
+}
+
+// Pega os processos descritos na tabela e os coloca num tipo de área de espera
+processo **parseTabela()
+{
+    FILE *tabela = fopen("tabela.txt", "r");
+    char *linha = NULL;
+    short int flag = 1; // marca se a primeira linha da tabela ainda não foi lida
     int i = 0;
     size_t tam_linha = 0;
     int numero_processos;
-    processo **espera; 
-    if (tabela  == NULL)
+    processo **espera;
+    if (tabela == NULL)
     {
         printf("Tabela não encontrada \n");
         return NULL;
     }
-    
-    while (getline(&linha,&tam_linha,tabela)!=-1)
+
+    while (getline(&linha, &tam_linha, tabela) != -1)
     {
-        if(flag){
+        if (flag)
+        {
             numero_processos = atoi(linha);
-            espera = (processo **)malloc(numero_processos*sizeof(processo*));//cria a área de espera
+            espera = (processo **)malloc(numero_processos * sizeof(processo *)); // cria a área de espera
             flag = 0;
             continue;
         }
         espera[i] = setProcesso(linha);
         i++;
     }
-    
+
     fclose(tabela);
-    return  espera;
     
+    bubbleSort(espera, numero_processos);
+    return espera;
 }
 int main(int argc, char const *argv[])
-{   
+{
+
+    processo **areaEspera;
     
-    processo** p;
-    p = parseTabela();
-    printf("%d", p[0]->pid);
+    areaEspera = parseTabela();
+
 
     return 0;
 }
